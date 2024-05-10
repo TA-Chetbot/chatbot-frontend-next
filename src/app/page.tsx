a74bdd7262476be348e292df6e62b94af9e7a39e
Adrian Utomo <adrianhakimutomo@gmail.com>
2024-05-11 00:02:42 +0700

    chore: change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
       <div className="background-pattern w-screen h-screen">
         <div className="w-full h-full flex flex-col justify-center items-center gap-[20px] text-center">
-          <h1 className="text-black font-bold text-7xl">Tugas Akhir Chatbot Swarm</h1>
+          <h1 className="text-black font-bold text-7xl">Tugas Akhir Chatbot Swarm Made with Next</h1>
           <p className="text-xl text-slate-800">Click on the bottom right icon to toggle the Chatbot Interface</p>
         </div>
         <React.Suspense fallback={<div>Loading...</div>}>
